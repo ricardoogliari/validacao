f88141149c36e6524c85b9052cc65b137c515fdb
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2026-09-12 21:21:21 -0300

    add error capture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:validacao/firebase_options.dart';
@@ -11,6 +13,18 @@ const clientId =
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // 1. Captura erros do Flutter (Ex: falhas de renderização, UI)
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    // Ou Sentry.captureException(errorDetails.exception);
+  };
+
+  // 2. Captura erros assíncronos do Dart fora do Flutter (Ex: Isolates, Promessas não tratadas)
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(
     ChangeNotifierProvider(
